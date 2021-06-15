3c5eb2e2a2214c180d26d9f8cef6e95845a903d8
Viviansol <viviansan93@gmail.com>
2021-06-14 22:09:06 -0300

    primeira versao

---
+++ script.js
@@ -0,0 +1,13 @@
+//exercício 
+for( let i = 0; i <= 5; i++){
+        console.log("Olá, mundo")
+}
+    
+//numeros ímpares
+
+
+//tabuada
+
+for(let i= 0; i <= 10; i++){
+    console.log(i + ' ' + "x 7 = " + i*7 )
+}
