018e49215188d768da656afbd187854a679c93eb
Vivian Sanches <84527029+Viviansol@users.noreply.github.com>
2021-06-14 22:19:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+//exercício 
+for( let i = 0; i <= 5; i++){
+        console.log("Olá, mundo")
+}
+    
+//numeros ímpares
+for(let impar = 1; impar <= 10; impar++){
+    if(impar%2!=0){
+        console.log(impar)
+    }
+}
+
+//tabuada
+
+for(let i= 0; i <= 10; i++){
+    console.log(i + ' ' + "x 7 = " + i*7 )
+}
